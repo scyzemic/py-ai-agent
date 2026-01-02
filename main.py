5515e42b9dd87312488f02ffdf68e1cc28531b34
scyzemic <11906352+scyzemic@users.noreply.github.com>
2026-01-02 10:02:50 -0500

    Add dotenv support and API key handling; update .gitignore and add VSCode settings

--- main.py
+++ main.py
@@ -1,3 +1,23 @@
+import os
+from dotenv import load_dotenv
+from google import genai
+
+load_dotenv()
+api_key = os.environ.get("GEMINI_API_KEY")
+
+if not api_key:
+    raise RuntimeError("GEMINI_API_KEY environment variable not set.")
+
+client = genai.Client(api_key=api_key)
+
+generated_content_response = client.models.generate_content(
+    model="gemini-2.5-flash",
+    contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.",
+)
+
+print(generated_content_response.text)
+
+
 def main():
     print("Hello from py-ai-agent!")
 
