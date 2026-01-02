eb166e4eec2498f1e352ce1d0211fe6d9d5fff5e
scyzemic <11906352+scyzemic@users.noreply.github.com>
2026-01-02 10:15:21 -0500

    Refactor user prompt handling and improve response metadata checks

--- main.py
+++ main.py
@@ -10,12 +10,23 @@ if not api_key:
 
 client = genai.Client(api_key=api_key)
 
+user_prompt = "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+
+print(f"User prompt: {user_prompt}")
+
 generated_content_response = client.models.generate_content(
-    model="gemini-2.5-flash",
-    contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.",
+    model="gemini-2.5-flash", contents=user_prompt
 )
 
-print(generated_content_response.text)
+usage_metadata = generated_content_response.usage_metadata
+if not usage_metadata:
+    raise RuntimeError("No usage metadata found in the response.")
+
+print(f"Prompt tokens: {usage_metadata.prompt_token_count}")
+
+print(f"Response tokens: {usage_metadata.candidates_token_count}")
+
+print(f"Response:\n{generated_content_response.text}")
 
 
 def main():
